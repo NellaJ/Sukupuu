1c87aeb4256713a2d71db04f20a504ca5e83490f
NellaJ <nella.junna@helsinki.fi>
2015-10-04 00:00:14 +0300

    uusi metodi

--- Sukupuu/src/main/java/sukupuu/piirustus/Nelio.java
+++ Sukupuu/src/main/java/sukupuu/piirustus/Nelio.java
@@ -10,16 +10,23 @@ import java.awt.Graphics;
 public class Nelio extends Kuvio {
 
     private int sivunPituus;
+    private boolean vari;
 
     public Nelio(int x, int y, int sivunPituus) {
         super(x, y);
         this.sivunPituus = sivunPituus;
+        this.vari = false;
     }
 
     public int getSivunPituus() {
         return sivunPituus;
     }
 
+    @Override
+    public void varita(Graphics graphics) {
+        graphics.fillRect(super.getX(), super.getY(), sivunPituus, sivunPituus);
+    }
+    
     @Override
     public void piirra(Graphics graphics) {
         graphics.drawRect(super.getX(), super.getY(), sivunPituus, sivunPituus);
