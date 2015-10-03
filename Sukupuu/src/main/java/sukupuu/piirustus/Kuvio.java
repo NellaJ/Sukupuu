30f27116523b82e48e5a4a87d3333bab73453bbe
NellaJ <nella.junna@helsinki.fi>
2015-10-03 23:59:52 +0300

    uusi metodi

--- Sukupuu/src/main/java/sukupuu/piirustus/Kuvio.java
+++ Sukupuu/src/main/java/sukupuu/piirustus/Kuvio.java
@@ -9,6 +9,7 @@ public abstract class Kuvio {
 
     private int x;
     private int y;
+    private boolean vari;
 
     public Kuvio(int x, int y) {
         this.x = x;
@@ -23,5 +24,16 @@ public abstract class Kuvio {
         return y;
     }
 
+    public void setVari(boolean vari) {
+        this.vari = vari;
+    }
+
+    public boolean isVari() {
+        return vari;
+    }
+
     public abstract void piirra(Graphics graphics);
+    
+    public abstract void varita(Graphics graphics);
 }
+    
