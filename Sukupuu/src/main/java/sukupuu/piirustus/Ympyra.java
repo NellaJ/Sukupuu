3fcba9da0472a019d6faabfc55155b3337bac70a
NellaJ <nella.junna@helsinki.fi>
2015-10-04 00:00:27 +0300

    uusi metodi

--- Sukupuu/src/main/java/sukupuu/piirustus/Ympyra.java
+++ Sukupuu/src/main/java/sukupuu/piirustus/Ympyra.java
@@ -9,16 +9,23 @@ import java.awt.Graphics;
 public class Ympyra extends Kuvio {
 
     private int halkaisija;
+    private boolean vari;
 
     public Ympyra(int x, int y, int halkaisija) {
         super(x, y);
         this.halkaisija = halkaisija;
+        this.vari = false;
     }
 
     public int getHalkaisija() {
         return halkaisija;
     }
 
+    @Override
+    public void varita(Graphics graphics) {
+        graphics.fillOval(super.getX(), super.getY(), halkaisija, halkaisija);
+    }
+    
     @Override
     public void piirra(Graphics graphics) {
         graphics.drawOval(super.getX(), super.getY(), halkaisija, halkaisija);
