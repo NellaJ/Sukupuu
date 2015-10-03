ad7b7664ec1cf7291285a11fd99404564870f117
NellaJ <nella.junna@helsinki.fi>
2015-10-03 23:59:12 +0300

    pari testia

--- Sukupuu/src/test/java/sukupuu/piirustus/PiirustusLogiikkaTest.java
+++ Sukupuu/src/test/java/sukupuu/piirustus/PiirustusLogiikkaTest.java
@@ -61,4 +61,48 @@ public class PiirustusLogiikkaTest {
         assertEquals(logiikka.getHenkilonKuvio().get(henkilot.get(1).getNimi()).getY(), logiikka.laskeY(henkilot.get(1)));
     }
    
+    @Test
+    public void laskeeXOikein() {
+        assertEquals(logiikka.laskeX(3), 700);
+    }
+    
+    @Test
+    public void laskeeYOikein() {
+        Henkilo tyyppi = new Henkilo("Tyyppi", 20, null, true, Sukupuoli.MIES);
+        tyyppi.setSukupolvi(4);
+        assertEquals(logiikka.laskeY(tyyppi), 800);
+    }
+    @Test
+    public void korkeusOikein() {
+        assertEquals(logiikka.laskeKorkeus(), 60);
+    }
+    
+    @Test
+    public void toinenXMetodiToimiiOikein() {
+        Henkilo mies2 = new Henkilo("M2", 40, "Sairas", true, Sukupuoli.MIES);    //indeksi 0
+        mies2.setSukupolvi(1);
+        Henkilo nainen2 = new Henkilo("N2", 30, "Terve", false, Sukupuoli.NAINEN); //indeksi 1
+        nainen2.setSukupolvi(1);
+        mies2.setPuoliso(nainen2);
+        nainen2.setPuoliso(mies2);
+        henkilot.add(mies2);    //indeksi 2
+        henkilot.add(nainen2);  //indeksi 3
+        logiikka = new Piirustuslogiikka(henkilot);
+        logiikka.keraaSukupolvet();
+        assertEquals(logiikka.kuvionXViivanKoordinaatiksi(nainen2), 730);
+    }
+    @Test
+    public void toinenYMetodiToimiiOikein() {
+        Henkilo mies2 = new Henkilo("M2", 40, "Sairas", true, Sukupuoli.MIES);    //indeksi 0
+        mies2.setSukupolvi(3);
+        Henkilo nainen2 = new Henkilo("N2", 30, "Terve", false, Sukupuoli.NAINEN); //indeksi 1
+        nainen2.setSukupolvi(3);
+        mies2.setPuoliso(nainen2);
+        nainen2.setPuoliso(mies2);
+        henkilot.add(mies2);    //indeksi 2
+        henkilot.add(nainen2);  //indeksi 3
+        logiikka = new Piirustuslogiikka(henkilot);
+        logiikka.keraaSukupolvet();
+        assertEquals(logiikka.kuvionYViivanKoordinaatiksi(mies2), 630);
+    }
 }
