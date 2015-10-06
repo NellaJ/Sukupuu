30f05685cb1e50543471362f8be8a8e5dc2d7943
NellaJ <nella.junna@helsinki.fi>
2015-10-06 20:52:32 +0300

    sairaus ja mutaatio muutettu

--- Sukupuu/src/test/java/sukupuu/piirustus/PiirustusLogiikkaTest.java
+++ Sukupuu/src/test/java/sukupuu/piirustus/PiirustusLogiikkaTest.java
@@ -18,13 +18,15 @@ public class PiirustusLogiikkaTest {
 
     Piirustuslogiikka logiikka;
     ArrayList<Henkilo> henkilot = new ArrayList<>();
+    ArrayList<String> mutaatiotTyhja = new ArrayList<>();
 
     @Before
     public void setUp() {
-        Henkilo mies = new Henkilo("M", 40, "Sairas", true, Sukupuoli.MIES);    //indeksi 0
+        
+        Henkilo mies = new Henkilo("M", 40, true, mutaatiotTyhja, Sukupuoli.MIES);    //indeksi 0
         mies.setSukupolvi(1);
         henkilot.add(mies);
-        Henkilo nainen = new Henkilo("N", 30, "Terve", false, Sukupuoli.NAINEN); //indeksi 1
+        Henkilo nainen = new Henkilo("N", 30, false, mutaatiotTyhja, Sukupuoli.NAINEN); //indeksi 1
         nainen.setSukupolvi(1);
         henkilot.add(nainen);
         logiikka = new Piirustuslogiikka(henkilot);
@@ -68,7 +70,7 @@ public class PiirustusLogiikkaTest {
     
     @Test
     public void laskeeYOikein() {
-        Henkilo tyyppi = new Henkilo("Tyyppi", 20, null, true, Sukupuoli.MIES);
+        Henkilo tyyppi = new Henkilo("Tyyppi", 20, false, mutaatiotTyhja, Sukupuoli.MIES);
         tyyppi.setSukupolvi(4);
         assertEquals(logiikka.laskeY(tyyppi), 800);
     }
@@ -79,9 +81,9 @@ public class PiirustusLogiikkaTest {
     
     @Test
     public void toinenXMetodiToimiiOikein() {
-        Henkilo mies2 = new Henkilo("M2", 40, "Sairas", true, Sukupuoli.MIES);    //indeksi 0
+        Henkilo mies2 = new Henkilo("M2", 40, true, mutaatiotTyhja, Sukupuoli.MIES);    //indeksi 0
         mies2.setSukupolvi(1);
-        Henkilo nainen2 = new Henkilo("N2", 30, "Terve", false, Sukupuoli.NAINEN); //indeksi 1
+        Henkilo nainen2 = new Henkilo("N2", 30, false, mutaatiotTyhja, Sukupuoli.NAINEN); //indeksi 1
         nainen2.setSukupolvi(1);
         mies2.setPuoliso(nainen2);
         nainen2.setPuoliso(mies2);
@@ -93,9 +95,9 @@ public class PiirustusLogiikkaTest {
     }
     @Test
     public void toinenYMetodiToimiiOikein() {
-        Henkilo mies2 = new Henkilo("M2", 40, "Sairas", true, Sukupuoli.MIES);    //indeksi 0
+        Henkilo mies2 = new Henkilo("M2", 40, true, mutaatiotTyhja, Sukupuoli.MIES);    //indeksi 0
         mies2.setSukupolvi(3);
-        Henkilo nainen2 = new Henkilo("N2", 30, "Terve", false, Sukupuoli.NAINEN); //indeksi 1
+        Henkilo nainen2 = new Henkilo("N2", 30, false, mutaatiotTyhja, Sukupuoli.NAINEN); //indeksi 1
         nainen2.setSukupolvi(3);
         mies2.setPuoliso(nainen2);
         nainen2.setPuoliso(mies2);
