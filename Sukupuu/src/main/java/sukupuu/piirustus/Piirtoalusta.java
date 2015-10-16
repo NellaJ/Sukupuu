1a8384c3d34979b3eb1f4d66a381d53f7bf27624
NellaJ <nella.junna@helsinki.fi>
2015-10-16 12:32:21 +0300

    Update Piirtoalusta.java

--- Sukupuu/src/main/java/sukupuu/piirustus/Piirtoalusta.java
+++ Sukupuu/src/main/java/sukupuu/piirustus/Piirtoalusta.java
@@ -39,7 +39,7 @@ public class Piirtoalusta extends JPanel {
                 kuvio.varita(graphics);
             }
         }
-        graphics2.drawString("TestiTeksti", 25, 25);
+     //   graphics2.drawString("TestiTeksti", 25, 25);
     }
 
 }
